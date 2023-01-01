ec248a3b9f3db398a9532cd84d57e24d6dc90148
SHREYAS PRABHU <shreyasprabhu.cb@gmail.com>
2023-01-01 16:34:37 +0530

    initial commit

---
+++ animehub/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './Logo.jsx';
+
+function App(){
+    return (
+        <div>
+            <Logo />
+            <h1>Hello world!</h1>
+
+        </div>
+    )
+}
+export default App;
